d72292d2173c7f4a28dcbf170a9d0f1143d8beab
shiori-nambu <shiori.nambu@esm.co.jp>
2022-10-12 15:52:32 +0900

    #4 Lambdaを作成する

--- infra/lib/types.ts
+++ infra/lib/types.ts
@@ -1,12 +1,37 @@
 import { StackProps } from "aws-cdk-lib";
+import { Repository } from "aws-cdk-lib/aws-ecr";
 import { UserPool } from "aws-cdk-lib/aws-cognito";
+import { DockerImageFunction } from "aws-cdk-lib/aws-lambda";
 import { Table } from "aws-cdk-lib/aws-dynamodb";
 import { MappingTemplate } from "@aws-cdk/aws-appsync-alpha";
+export type WeatherInfoCreateProps = LambdaResolverAttachable & {
+  image: Image;
+  openWeatherMapApiKey: string;
+};
+
 export type AppsyncCreateProps = {
   userPool: UserPool;
 };
 
 export type DynamodbCreateProps = DynamodbResolverAttachable;
+
+export type Image = {
+  repository: Repository;
+  tag: string;
+};
+
+export type LambdaResolverAttachable = {
+  attachLambdaResolver: (
+    dataSourceName: string,
+    lambda: DockerImageFunction,
+    operations: {
+      typeName: string;
+      fieldName: string;
+      requestMappingTemplate?: MappingTemplate;
+      responseMappingTemplate?: MappingTemplate;
+    }[]
+  ) => void;
+};
 
 export type DynamodbResolverAttachable = {
   attachDynamodbResolver: (
